64f97f2b2066fcfb780003f9d5678fd1194c1ab9
alissonpedrina <pedrina.alisson@gmail.com>
2015-04-28 19:36:38 -0400

    primeiro commit

---
+++ altaperformance-geral/src/main/java/com/wordpress/alissonpedrina/altaperformance/App.java
@@ -0,0 +1,13 @@
+package com.wordpress.alissonpedrina.altaperformance;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
